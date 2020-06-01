1401dc4842998a71a6c99b07d73fbdbe5d77ce0a
Rachana <podarallarachana@ufl.edu>
2020-06-01 10:36:39 -0500

    Added floor and cube blocks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,105 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { TestScene } from "./scene";
+
 import "./styles/main.scss";
 
-TestScene();
+var camera, controls, scene, renderer;
+
+init();
+//render(); // remove when using next line for animation loop (requestAnimationFrame)
+animate();
+
+function init() {
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xffffff);
+  scene.fog = new THREE.FogExp2(0xffffff, 0.002);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+  document.body.appendChild(renderer.domElement);
+
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.set(400, 200, 0);
+
+  // controls
+
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+
+  controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+  controls.dampingFactor = 0.05;
+
+  controls.screenSpacePanning = false;
+
+  controls.minDistance = 100;
+  controls.maxDistance = 500;
+
+  controls.maxPolarAngle = Math.PI / 2;
+
+  // lights
+  // White directional light at half intensity shining from the top.
+  var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  scene.add(directionalLight);
+
+  var light = new THREE.AmbientLight(0x404040); // soft white light
+  scene.add(light);
+
+  // world
+  var geometry = new THREE.BoxBufferGeometry(10, 10, 10, 4, 1);
+  var material = new THREE.MeshPhongMaterial({
+    color: 0x2ee860,
+    flatShading: true,
+  });
+
+  for (var i = 0; i < 500; i++) {
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = Math.random() * 1600 - 800;
+    mesh.position.y = 0;
+    mesh.position.z = Math.random() * 1600 - 800;
+    mesh.updateMatrix();
+    mesh.matrixAutoUpdate = false;
+    scene.add(mesh);
+  }
+
+  //Create floor
+  var planeGeometry = new THREE.PlaneBufferGeometry(500, 500, 1, 1);
+  var planeMaterial = new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    side: THREE.DoubleSide,
+  });
+  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.rotateX(-Math.PI / 2);
+  scene.add(plane);
+
+  // resizable canvas
+  window.addEventListener("resize", onWindowResize, false);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+
+  render();
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
